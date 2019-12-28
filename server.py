6cd3607d2ba93e9938278d6f8d7d0e00a3d3340f
JugurthaT <JugurthaT@gmail.com>
2019-12-28 17:35:11 -0500

    2nd

---
+++ server.py
@@ -0,0 +1,18 @@
+import socket
+import sys
+
+
+def Server():
+    """
+    Here we create the server listening socket
+    :return:
+    """
+    print("Starting the server socket !!!")
+    server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, socket.SO_REUSEPORT, 1)
+    if len(sys.argv) !=3:
+        print("Missing arguments")
+        exit()
+
+
+if __name__ == '__main__': Server()
